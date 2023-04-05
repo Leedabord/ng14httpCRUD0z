24bd923b78504c46faa5c2b457044b3e3ecf680e
Leedabord <gwforumlakers@gmail.com>
2023-04-04 17:16:33 -0700

    2 files modified

--- src/app/student.ts
+++ src/app/student.ts
@@ -1,7 +1,11 @@
-export interface Student {
+export interface Student2 {
   id: number;
   firstName: string;
   lastName: string;
   gender: string;
   age: number;
+}
+
+export interface Student {
+  Payee: string; TransDate: string; Amount: number; Category: string; subCat: string; Notes: string;
 }
--- src/app/students.service.ts
+++ src/app/students.service.ts
@@ -10,18 +10,23 @@ export class StudentsService {
   constructor(private httpClient: HttpClient) {}
 
   get(): Observable<Student[]> {
+    return this.httpClient.get<Student[]>(
+      'https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Transactions?api_key=key66fQg5IghIIQmb');
+  }
+
+/**  
+  get(): Observable<Student[]> {
     return this.httpClient.get<Student[]>('http://localhost:3000/students');
   }
 
   create(payload: Student) {
-    return this.httpClient.post<Student>(
-      'http://localhost:3000/students',
+    return this.httpClient.get<Student[]>('http://localhost:3000/students'),
       payload
     );
   }
 
   getById(id: number): Observable<Student> {
-    return this.httpClient.get<Student>(`http://localhost:3000/students/${id}`);
+   return this.httpClient.get<Student>(`http://localhost:3000/students/${id}`);
   }
 
   update(payload: Student): Observable<Student> {
@@ -34,4 +39,6 @@ export class StudentsService {
   delete(id: number) {
     return this.httpClient.delete(`http://localhost:3000/students/${id}`);
   }
+**/
+
 }
