9570cc8f37c11359e89a83fa41a0bec99e33b655
Leedabord <gwforumlakers@gmail.com>
2023-04-07 16:47:28 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './students.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang14-material-ui-crud';
+
+posts : any;
+constructor(private httpService: HttpService) { }
+
+ngOnInit() {
+	this.httpService.getPosts().subscribe(
+	(response) => { this.posts = response; },
+	(error) => { console.log(error); });
+}
+
 }
