179cf5e443dc8672426ab0f1b90e707535246178
Leedabord <gwforumlakers@gmail.com>
2023-04-07 16:56:42 -0700

    2 files modified

--- src/app/students.service.ts
+++ src/app/students.service.ts
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
   
   private url =
-  'https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Students?api_key=key66fQg5IghIIQmb';
+  'https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Scores?api_key=key66fQg5IghIIQmb';
 //  'https://my-json-server.typicode.com/JSGund/XHR-Fetch-Request-JavaScript/posts';
   
   constructor(private http: HttpClient) { }
